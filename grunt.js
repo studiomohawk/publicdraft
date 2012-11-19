4bab2de23e57f6697bb27c3f9bd0bfb2d1e773fd
Yuya Saito <studiomohawk@gmail.com>
2012-11-19 11:36:20 +0900

    new theme

---
+++ grunt.js
@@ -0,0 +1,91 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		csso: {
+			dist: {
+				src: 'asset/style/style.css',
+				dest: 'asset/style/style.css'
+			}
+		},
+		min: {
+			dist: {
+				src: ['asset/script/script.js'],
+				dest: 'asset/script/script.js'
+			}
+		},
+		stylus: {
+			compile: {
+				options: {
+					'paths': ['asset/style'],
+					'compress': false
+				},
+				files: {
+					'asset/style/style.css': 'asset/style/style.styl'
+				}
+			}
+		},
+		img: {
+			dist: {
+				src: 'asset/image'
+			}
+		},
+		optipng: {
+			args: ["-o5"]
+		},
+		jekyll: {
+			preview: {
+				auto: true,
+				server: true
+			}
+		},
+		aws: '<json:grunt-aws.json>',
+		s3: {
+			key: '<%= aws.key %>',
+			secret: '<%= aws.secret %>',
+			bucket: 'publicdraft.studiomohawk.com',
+			access: 'public-read',
+			upload: [
+				{
+					src: '_site/**/*',
+					dest: '.'
+				},
+				{
+					src: '_site/asset/style/*.css',
+					dest: 'asset/style/',
+					gzip: true
+				},
+				{
+					src: '_site/asset/script/*.js',
+					dest: 'asset/script/',
+					gzip: true
+				}
+			]
+		},
+		watch: {
+			css: {
+				files: ['asset/style/*.styl'],
+				tasks: ['stylus']
+			}
+		}
+	});
+
+	// Load plugin
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-csso');
+	grunt.loadNpmTasks('grunt-img');
+	grunt.loadNpmTasks('grunt-jekyll');
+	grunt.loadNpmTasks('grunt-s3');
+
+	// Default task
+	grunt.registerTask('default', 'watch');
+	// CSS Build task
+	grunt.registerTask('cssbuild', ['stylus','csso']);
+	// JavaScript Build task
+	grunt.registerTask('jsbuild', ['min']);
+	// development task
+	grunt.registerTask('dev', ['jekyll', 'watch']);
+	// depoy task
+	grunt.registerTask('deploy', ['cssbuild','s3']);
+
+};
